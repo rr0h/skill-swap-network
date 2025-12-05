9c59810cf9a6c1276505a510133ae7cde8b11061
Rohit <119391810+rr0h@users.noreply.github.com>
2025-12-05 14:46:49 +0530

    Add notification admin configuration

---
+++ notifications/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import Notification
+
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    """Notification admin"""
+    list_display = ['user', 'title', 'notification_type', 'is_read', 'created_at']
+    list_filter = ['notification_type', 'is_read', 'created_at']
+    search_fields = ['user__username', 'title', 'message']
+    date_hierarchy = 'created_at'
+    readonly_fields = ['created_at']
+    
+    actions = ['mark_as_read', 'mark_as_unread']
+    
+    def mark_as_read(self, request, queryset):
+        queryset.update(is_read=True)
+    mark_as_read.short_description = "Mark selected as read"
+    
+    def mark_as_unread(self, request, queryset):
+        queryset.update(is_read=False)
+    mark_as_unread.short_description = "Mark selected as unread"
